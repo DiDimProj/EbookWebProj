b46fe6b31380a72b11cf31a8f3bf32e581b08d81
dasom923 <30401917+dasom923@users.noreply.github.com>
2017-07-25 17:51:10 +0900

    commit.2017.07.25

--- EbookWebProj/book.sql
+++ EbookWebProj/book.sql
@@ -1,83 +1,92 @@
+CREATE TABLE GENRE(
+   GENRENUM  	VARCHAR2(20)      PRIMARY KEY,
+   GENRENAME   	VARCHAR2(20)   NOT NULL,
+   TAG         	VARCHAR2 (20)  
+);
+
 SELECT   *
 FROM   GENRE ;
 
+DROP TABLE GENRE ;
+
+
 INSERT   INTO   GENRE 
-VALUES (01,'소설',null);
+VALUES ('01','소설',null);
 INSERT   INTO   GENRE 
-VALUES (01-1,'소설','추리');
+VALUES ('01-1','소설','추리');
 INSERT   INTO   GENRE 
-VALUES (01-2,'소설','호러');
+VALUES ('01-2','소설','호러');
 INSERT   INTO   GENRE 
-VALUES (01-3,'소설','연애');
+VALUES ('01-3','소설','연애');
 INSERT   INTO   GENRE 
-VALUES (02,'기술',null);
+VALUES ('02','기술',null);
 INSERT   INTO   GENRE 
-VALUES (02-1,'기술','IT');
+VALUES ('02-1','기술','IT');
 INSERT   INTO   GENRE 
-VALUES (02-2,'기술','전자/전기');
+VALUES ('02-2','기술','전자/전기');
 INSERT   INTO   GENRE 
-VALUES (02-3,'기술','건축');
+VALUES ('02-3','기술','건축');
 INSERT   INTO   GENRE 
-VALUES (03,'만화',null);
+VALUES ('03','만화',null);
 INSERT   INTO   GENRE 
-VALUES (03-1,'만화','코믹');
+VALUES ('03-1','만화','코믹');
 INSERT   INTO   GENRE 
-VALUES (03-2,'만화','성인');
+VALUES ('03-2','만화','성인');
 INSERT   INTO   GENRE 
-VALUES (03-3,'만화','판타지');
+VALUES ('03-3','만화','판타지');
 INSERT   INTO   GENRE 
-VALUES (04,'경제/경영',null);
+VALUES ('04','경제/경영',null);
 INSERT   INTO   GENRE 
-VALUES (04-1,'경제/경영','마케팅');
+VALUES ('04-1','경제/경영','마케팅');
 INSERT   INTO   GENRE 
-VALUES (04-2,'경제/경영','재테크');
+VALUES ('04-2','경제/경영','재테크');
 INSERT   INTO   GENRE 
-VALUES (04-3,'경제/경영','창업');
+VALUES ('04-3','경제/경영','창업');
 INSERT   INTO   GENRE 
-VALUES (05,'순수과학',null);
+VALUES ('05','순수과학',null);
 INSERT   INTO   GENRE 
-VALUES (05-1,'순수과학','수학');
+VALUES ('05-1','순수과학','수학');
 INSERT   INTO   GENRE 
-VALUES (05-2,'순수과학','천체/물리');
+VALUES ('05-2','순수과학','천체/물리');
 INSERT   INTO   GENRE 
-VALUES (05-3,'순수과학','화학');
+VALUES ('05-3','순수과학','화학');
 INSERT   INTO   GENRE 
-VALUES (06,'예체능',null);
+VALUES ('06','예체능',null);
 INSERT   INTO   GENRE 
-VALUES (06-1,'예체능','음악');
+VALUES ('06-1','예체능','음악');
 INSERT   INTO   GENRE 
-VALUES (06-2,'예체능','미술');
+VALUES ('06-2','예체능','미술');
 INSERT   INTO   GENRE 
-VALUES (06-3,'예체능','체육');
+VALUES ('06-3','예체능','체육');
 INSERT   INTO   GENRE 
-VALUES (07,'자기계발',null);
+VALUES ('07','자기계발',null);
 INSERT   INTO   GENRE 
-VALUES (07-1,'자기계발','인간관계');
+VALUES ('07-1','자기계발','인간관계');
 INSERT   INTO   GENRE 
-VALUES (07-2,'자기계발','성공/처세');
+VALUES ('07-2','자기계발','성공/처세');
 INSERT   INTO   GENRE 
-VALUES (07-3,'자기계발','화술/협상');
+VALUES ('07-3','자기계발','화술/협상');
 INSERT   INTO   GENRE 
-VALUES (08,'역사',null);
+VALUES ('08','역사',null);
 INSERT   INTO   GENRE 
-VALUES (08-1,'역사','한국사');
+VALUES ('08-1','역사','한국사');
 INSERT   INTO   GENRE 
-VALUES (08-2,'역사','세계사');
+VALUES ('08-2','역사','세계사');
 INSERT   INTO   GENRE 
-VALUES (08-3,'역사','신화');
+VALUES ('08-3','역사','신화');
 INSERT   INTO   GENRE 
-VALUES (09,'종교',null);
+VALUES ('09','종교',null);
 INSERT   INTO   GENRE 
-VALUES (09-1,'종교','불교');
+VALUES ('09-1','종교','불교');
 INSERT   INTO   GENRE 
-VALUES (09-2,'종교','천주교');
+VALUES ('09-2','종교','천주교');
 INSERT   INTO   GENRE 
-VALUES (09-3,'종교','기독교');
+VALUES ('09-3','종교','기독교');
 INSERT   INTO   GENRE 
-VALUES (10,'언어',null);
+VALUES ('10','언어',null);
 INSERT   INTO   GENRE 
-VALUES (10-1,'언어','한국어');
+VALUES ('10-1','언어','한국어');
 INSERT   INTO   GENRE 
-VALUES (10-2,'언어','영어');
+VALUES ('10-2','언어','영어');
 INSERT   INTO   GENRE 
-VALUES (10-3,'언어','중국어');
+VALUES ('10-3','언어','중국어');
