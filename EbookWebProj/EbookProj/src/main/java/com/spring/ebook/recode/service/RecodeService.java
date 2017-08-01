86254ea79825ae20e09afb6c3955def0a06a84a4
HSuHyun <30396968+HSuHyun@users.noreply.github.com>
2017-08-01 17:42:40 +0900

    0801 recode setting
    기본세팅

--- EbookWebProj/EbookProj/src/main/java/com/spring/ebook/recode/service/RecodeService.java
+++ EbookWebProj/EbookProj/src/main/java/com/spring/ebook/recode/service/RecodeService.java
@@ -1,5 +1,11 @@
 package com.spring.ebook.recode.service;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.spring.ebook.model.recode.dao.RecodeDao;
 
+@Service("recodeService")
 public class RecodeService {
-
+	
+	@Resource(name="recodeDao")
+	private RecodeDao dao;
 }
