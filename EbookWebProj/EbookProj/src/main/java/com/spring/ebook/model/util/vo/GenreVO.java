fe4bc440bd00764dfaae75db2e3a84fb7e5a3cb0
thflf <thflf@naver.com>
2017-08-09 20:00:33 +0900

    0809 - chart완성중
    수현아 칭찬해죠

--- EbookWebProj/EbookProj/src/main/java/com/spring/ebook/model/util/vo/GenreVO.java
+++
@@ -1,6 +0,0 @@
-package com.spring.ebook.model.util.vo;
-
-public class GenreVO {
-
-	
-}
--- EbookWebProj/EbookProj/src/main/java/com/spring/ebook/model/util/vo/ReadchartVO.java
+++ EbookWebProj/EbookProj/src/main/java/com/spring/ebook/model/util/vo/ReadchartVO.java
@@ -1,5 +1,43 @@
 package com.spring.ebook.model.util.vo;
 
 public class ReadchartVO {
-
+	private String	genrenum, genrename, tag;
+	private int		cnt;
+	public ReadchartVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ReadchartVO(String genrenum, String genrename, String tag, int cnt) {
+		super();
+		this.genrenum = genrenum;
+		this.genrename = genrename;
+		this.tag = tag;
+		this.cnt = cnt;
+	}
+	public String getGenrenum() {
+		return genrenum;
+	}
+	public void setGenrenum(String genrenum) {
+		this.genrenum = genrenum;
+	}
+	public String getGenrename() {
+		return genrename;
+	}
+	public void setGenrename(String genrename) {
+		this.genrename = genrename;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
 }
